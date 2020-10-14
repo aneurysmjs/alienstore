1dd25b7a64be84d4054f4a1e2c46ed34b121b74e
jero <aneurysmjs@outlook.com>
2020-10-14 22:44:11 +0300

    fix(types/alienstore): correct ReduxModule's typing
    infer properly reducers, actions and selectors
    
    fix #7

--- src/types/alienStore.ts
+++ src/types/alienStore.ts
@@ -7,6 +7,7 @@ import {
   ActionFromReducersMapObject,
   StateFromReducersMapObject,
 } from 'redux';
+import { ValueOf } from './helpers';
 
 // mapped the types of the reducers to produce state's shape type
 export type FullStoreShape<T> = {
@@ -16,15 +17,36 @@ export type FullStoreShape<T> = {
 export type ReducerMapper<U> = Partial<{ [K in keyof Partial<U>]: Reducer<U[K]> }>;
 
 /**
+ * Infers state from reducers map object and also its actions
+ * @template R object whose keys are reducers functions.
+ */
+type StateAndActionsFromReducersMapObject<
+  R extends ReducersMapObject<any, any>
+> = ReducersMapObject<StateFromReducersMapObject<R>, ActionFromReducersMapObject<R>>;
+
+/**
+ * A selector is a function that takes a piece of the state and returns it.
+ * @template S The type of state consumed by this selector.
+ */
+export type Selector<S> = (state: S) => ValueOf<S>;
+
+/**
+ * Object whose values are selector functions.
+ * @template S The type of state given to each selector.
+ */
+export type SelectorsMapObject<S> = {
+  [K: string]: Selector<S>;
+};
+
+/**
  * Every piece of the state its considered a Redux module.
+ * @template R object whose keys are reducers functions.
  */
 export interface ReduxModule<R extends ReducersMapObject<any, any> = ReducersMapObject<any, any>> {
   id: string;
-  reducers: ReducersMapObject<StateFromReducersMapObject<R>, ActionFromReducersMapObject<R>>;
+  reducers: StateAndActionsFromReducersMapObject<R>;
   actions: ActionCreatorsMapObject<ActionFromReducersMapObject<R>>;
-  selectors?: {
-    [K: string]: <S, R>(state: S) => R;
-  };
+  selectors?: SelectorsMapObject<StateFromReducersMapObject<R>>;
 }
 
 /**
